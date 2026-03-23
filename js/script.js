113fc0aff21117fe903a66fddc024534258f887f
Gael <gael027gg@gmail.com>
2026-03-23 17:32:53 -0600

    Pagina web de Halo

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function irSteam(){
+
+window.open("https://store.steampowered.com/search/?term=halo","_blank");
+
+}
+
+function irXbox(){
+
+window.open("https://www.xbox.com/es-MX/games/halo","_blank");
+
+}
